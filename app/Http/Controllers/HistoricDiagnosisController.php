0e0119ea2f0e52dc153bccf1a863bb0a0d8c467e
Rodrigo Uriarte <rodrigo.uriarte05@gmail.com>
2022-07-06 17:13:39 -0300

    retrieve data from Auth user

--- app/Http/Controllers/HistoricDiagnosisController.php
+++ app/Http/Controllers/HistoricDiagnosisController.php
@@ -5,18 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Resources\HistoricDiagnosisResource;
 use App\Models\HistoricDiagnosis;
 use App\Validators\GetHistoricDiagnosticsValidator;
+use Illuminate\Support\Facades\Auth;
 
 class HistoricDiagnosisController extends Controller
 {
     public function index(GetHistoricDiagnosticsValidator $request)
     {
         $limit = $this->getPaginationLimit($request);
-        $user_id = $request->input('user_id');
+        $user_id = Auth::user()->id;
 
         $historic_diagnostics = HistoricDiagnosis::query()
             ->where('user_id', $user_id)
-            ->paginate($limit)
-            ->withQueryString();
+            ->paginate($limit);
 
         return $this->response(HistoricDiagnosisResource::collection($historic_diagnostics));
     }
